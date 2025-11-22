9b72f9d1928966dbee4dc20173d827db52623745
Mueen <hejiy2002@gmail.com>
2025-11-23 02:24:15 +0300

    Improved

--- src/EX2/Roman.java
+++ src/EX2/Roman.java
@@ -8,67 +8,35 @@ public class Roman {
 
 
         LinkedHashMap<Integer, String> RomanValues = new LinkedHashMap<>();
+
         RomanValues.put(1000, "M");
+        RomanValues.put(900, "CM");
         RomanValues.put(500, "D");
+        RomanValues.put(400, "CD");
         RomanValues.put(100, "C");
+        RomanValues.put(90, "XC");
         RomanValues.put(50, "L");
+        RomanValues.put(40, "XL");
         RomanValues.put(10, "X");
+        RomanValues.put(9, "IX");
         RomanValues.put(5, "V");
+        RomanValues.put(4, "IV");
         RomanValues.put(1, "I");
 
+         String Answer ="";
+       for(Object a :RomanValues.keySet()) {
 
-       for(Object a :RomanValues.keySet()){
-           if (value== (4)){
-               return "IV";
-           }
-           if(value==9){
-               return "IX";
-           }
-           if (value >=40 && value <50){
-               System.out.print("XL");
-               value=value-40;
+           if (value / (int)a >= 1) {
+               int result = value / (int) a;
 
-           }if (value>=90&& value <100){
-               System.out.print("XC");
-               value=value-90;
-           }
-           if (value>=400&&value < 500){
-               System.out.print("CD");
-               value=value-400;
-           }if (value>=900 &&value<1000) {
-               System.out.print("CM");
-               value=value-900;
-           }
-           if (value/(int)a >=1){
-
-
-
-               int result = value/(int)a;
-
-
-
-
-               value=value-((int)a*result);
-               for (int i =0;i<result;i++){
-                   System.out.print(RomanValues.get(a));
+               for (int i = 0; i < result; i++) {
+                   Answer += RomanValues.get(a);
                }
-           } else if ((int)a/value==0) {
 
-               System.out.print(RomanValues.get(a));
-               System.out.println(" ");
-
-
+               value = value - ((int) a * result);
            }
        }
-
-
-
-
-
-
-
-
-        return "";
+        return Answer;
     }
 
     public static void main(String[] args) {
